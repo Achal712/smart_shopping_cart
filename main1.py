46a2a134a343369cc24b2cda015648c1b767e28e
Achal712 <96222497+Achal712@users.noreply.github.com>
2025-01-08 00:26:15 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # Constants
 IMAGE_URL = "http://172.20.10.4:81/capture"  # Replace with your dynamic image URL
-MODEL_PATH = "own_data.pt"
+MODEL_PATH = "z.pt"
 REFRESH_INTERVAL = 3  # Time in seconds
 TEMP_IMAGE_PATH = "temp_image.jpg"
 PRICES = {
